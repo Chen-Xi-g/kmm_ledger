dbf3f23d15e8b9b4ad9b1bec5974c25c6e9b4e29
高国峰 <ggf@ylean.com.cn>
2023-12-16 01:22:12 +0800

    fix: gitignore规则修改
    提交内容:
    - 新增账户相关接口存储库

---
+++ composeApp/src/commonMain/kotlin/core/data/repository/AccountRepositoryImpl.kt
@@ -0,0 +1,75 @@
+package core.data.repository
+
+import core.data.api.NetApi
+import core.data.dto.AgreementDto
+import core.data.dto.CodeImageDto
+import core.data.dto.TokenDto
+import core.data.net.ResNet
+import core.data.net.get
+import core.data.net.post
+import core.domain.entity.CodeImageEntity
+import core.domain.repository.AccountRepository
+import core.mappers.toAgreement
+import core.mappers.toCodeImageEntity
+import core.mappers.toToken
+
+/**
+ * 账户相关接口实现
+ *
+ * @author 高国峰
+ * @date 2023/12/16-01:11
+ */
+class AccountRepositoryImpl: AccountRepository{
+    override suspend fun codeImage(req: NetApi.AccountApi.CodeImage): ResNet<CodeImageEntity> {
+        return get<CodeImageDto>(req.url).toCodeImageEntity()
+    }
+
+    override suspend fun login(
+        username: String,
+        password: String,
+        code: String,
+        uuid: String
+    ): ResNet<String> {
+        val req = NetApi.AccountApi.Login(username, password, code, uuid)
+        return post<TokenDto>(req.url, req.toMap()).toToken()
+    }
+
+    override suspend fun register(
+        username: String,
+        email: String,
+        password: String,
+        code: String,
+        uuid: String
+    ): ResNet<String> {
+        val req = NetApi.AccountApi.Register(username, email, password, code, uuid)
+        return post(req.url, req.toMap())
+    }
+
+    override suspend fun forgotPwd(
+        username: String,
+        password: String,
+        confirmPassword: String,
+        code: String,
+        uuid: String
+    ): ResNet<String> {
+        val req = NetApi.AccountApi.ForgotPwd(username, password, confirmPassword, code, uuid)
+        return post(req.url, req.toMap())
+    }
+
+    override suspend fun activateAccount(
+        username: String,
+        code: String,
+        uuid: String
+    ): ResNet<String> {
+        val req = NetApi.AccountApi.ActivateAccount(username, code, uuid)
+        return post(req.url, req.toMap())
+    }
+
+    override suspend fun agreement(type: Int): ResNet<String> {
+        val req = NetApi.AccountApi.Agreement(type)
+        return get<AgreementDto>(req.url){
+            put("type", type.toString())
+        }.toAgreement()
+    }
+
+}
---
+++ composeApp/src/commonMain/kotlin/core/domain/repository/AccountRepository.kt
@@ -0,0 +1,74 @@
+package core.domain.repository
+
+import core.data.api.NetApi
+import core.data.dto.AgreementDto
+import core.data.net.ResNet
+import core.domain.entity.CodeImageEntity
+
+/**
+ * 账户相关接口
+ *
+ * @author 高国峰
+ * @date 2023/12/16-01:08
+ */
+interface AccountRepository {
+
+    /**
+     * 获取验证码
+     *
+     * @return 返回图片地址
+     */
+    suspend fun codeImage(req: NetApi.AccountApi.CodeImage = NetApi.AccountApi.CodeImage): ResNet<CodeImageEntity>
+
+    /**
+     * 登录
+     *
+     * @param code 输入的验证码
+     * @param uuid 验证码唯一ID
+     *
+     * @return 返回Token信息
+     */
+    suspend fun login(username: String, password: String, code: String, uuid: String): ResNet<String>
+
+    /**
+     * 注册
+     *
+     * @param username 用户名
+     * @param email 邮箱
+     * @param password 密码
+     * @param code 验证码
+     * @param uuid 验证码唯一ID
+     *
+     * @return 返回是否注册成功
+     */
+    suspend fun register(username: String, email: String, password: String, code: String, uuid: String): ResNet<String>
+
+    /**
+     * 忘记密码
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @param confirmPassword 确认密码
+     * @param code 验证码
+     * @param uuid 验证码唯一ID
+     *
+     * @return 发送修改密码确认邮箱
+     */
+    suspend fun forgotPwd(username: String, password: String, confirmPassword: String, code: String, uuid: String): ResNet<String>
+
+    /**
+     * 发送激活账户的邮件
+     *
+     * @param username 用户名
+     * @param code 验证码
+     * @param uuid 验证码唯一ID
+     */
+    suspend fun activateAccount(username: String, code: String, uuid: String): ResNet<String>
+
+    /**
+     * 获取系统协议
+     *
+     * @param type 协议类型 1:用户协议 2:隐私政策
+     */
+    suspend fun agreement(type: Int): ResNet<String>
+}
